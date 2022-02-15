c78171565fd82839caf45fc36f68a944a73c8a90
mozesachin02@gmail.com <mozesachin02@gmail.com>
2022-02-15 14:25:24 +0530

    store and carts functionality

---
+++ carts/context_processors.py
@@ -0,0 +1,17 @@
+
+from .models import Cart, cartItem
+from .views import __cart_id
+
+def counter(request):
+    cart_count = 0
+    if 'admin' in request.path:
+        return {}
+    else:
+        try:
+            cart = Cart.objects.filter(cart_id=__cart_id(request))
+            cart_items = cartItem.objects.all().filter(cart=cart[:1])
+            for cart_item in cart_items:
+                cart_count += cart_item.quantity
+        except Cart.DoesNotExist:
+            cart_count = 0
+    return dict(cart_count=cart_count)         
