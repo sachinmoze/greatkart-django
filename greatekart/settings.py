2e53979b8e669e13bbc7e70cce055a2f3e274222
mozesachin02@gmail.com <mozesachin02@gmail.com>
2022-02-22 23:19:37 +0530

    Django authentication commit

--- greatekart/settings.py
+++ greatekart/settings.py
@@ -134,3 +134,18 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+
+    messages.ERROR: 'danger',
+    50: 'critical',
+}
+
+#SMTP configuration
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'bablu.moze@gmail.com'
+EMAIL_HOST_PASSWORD = 'April*13041995'
+EMAIL_USE_TLS = True
